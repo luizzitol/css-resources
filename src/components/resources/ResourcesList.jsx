eb049b9e2ab521f164b87adeb4e2fe3f389af321
luizzitol <luizzitol@gmail.com>
2021-11-04 08:56:17 +0000

    Resources list structure outlined

---
+++ src/components/resources/ResourcesList.jsx
@@ -0,0 +1,11 @@
+// Takes an array of Resources as props and map through them to display a list
+
+function ResourcesList(props) {
+  return (
+    <ul>
+      <p>List of Resources</p>
+    </ul>
+  );
+}
+
+export default ResourcesList;
